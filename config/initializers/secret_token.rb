d1ad169b39478caa14a60224e93a1f52e4132a5d
Galy.Yang <galy.yang@gmail.com>
2014-01-20 12:09:05 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wechat::Application.config.secret_key_base = '62fa9e4cda0fe56a565e98707de83998b0838eeec9258642e0bf08fac548255842f95ef32f3592a203b585a1b6b97a27051018e7bfd5a4f7e371899ba9af6e03'
